017edf12e9e721c7c28d15ea61d77479e4ac4877
Anders Lindh Olsson <anders.lindholsson@esss.se>
2019-11-05 23:06:54 +0100

    clean /scripts/*.py in cnc_interface

--- src/cnc_interface/scripts/cnc_class.py
+++ src/cnc_interface/scripts/cnc_class.py
@@ -1,238 +1,247 @@
-import serial
+"""Class valid for interfacing XYZ cartesian CNC
+Future implementations might include control for other
+GCODE compatible systems
+
+See scattered todos
+"""
+
 import time
 import re
-import rospy
-from std_msgs.msg import String
+import serial
 from geometry_msgs.msg import Twist
 
-''' Class valid for interfacing XYZ cartesian CNC
-	Future implementations might include control for other 
-	GCODE compatible systems
-'''
 
-class cnc:
-	# regular expression for parsing GRBL status msgs
-	__pos_pattern__ = re.compile('.Pos:(\-?\d+\.\d+),(\-?\d+\.\d+),(\-?\d+\.\d+)')	
-	
-	def __init__(self):
-		self.s 		      =   None	# serial port object
-		self.abs_move     =   None	# GRBL has 2 movement modes, relative and absolute
-		# Default parameter values set in startup
-		self.baudrate 	  = 	 0
-		self.port 		  =     ''
-		self.acceleration =   	 0
-		self.x_max        =   	 0
-		self.y_max        =   	 0
-		self.z_max        =   	 0
-		self.defaultSpeed =      0	
-		self.x_max_speed  =   	 0
-		self.y_max_speed  =   	 0
-		self.z_max_speed  =   	 0
-		# number of steps per centimeter in each dimension
-		self.x_steps_mm   =   	 0
-		self.y_steps_mm   =   	 0
-		self.z_steps_mm   =   	 0
-		# machine idle
-		self.idle 		  =   True	
-		# vectors follow the format [X, Y, Z] where Z is assumed to be vertical
-		self.pos     = [0.0, 0.0, 0.0]   # current position
-		self.angular = [0.0, 0.0, 0.0]	 # angular coordinates
-		self.origin  = [0.0, 0.0, 0.0]	 # minimum coordinates
-		self.limits  = [0.0, 0.0, 0.0]	 # maximum coordinates
-	
-	def startup(self,port,baud, acc, maxx, maxy,maxz,spdf,spdx, spdy, spdz, stepsx, stepsy, stepsz):
-		""" initiate all CNC parameters readed from .launch file """
-		self.baudrate 	  =   baud
-		self.port 	      =   port
-		self.acceleration =    acc
-		self.x_max        =   maxx
-		self.y_max        =   maxy
-		self.z_max        =   maxz
-		self.defaultSpeed =   spdf
-		self.x_max_speed  =   spdx
-		self.y_max_speed  =   spdy
-		self.z_max_speed  =   spdz
-		self.x_steps_mm   = stepsx
-		self.y_steps_mm   = stepsy
-		self.z_steps_mm   = stepsz
-		self.limits  = [self.x_max, self.y_max, self.z_max]	
-		#initiates the serial port
-		self.s = serial.Serial(self.port, self.baudrate)
-		# set movement to Absolut coordinates
-		self.ensureMovementMode(True)
-		# start homing procedure
-		self.home()
-		# set the current position as the origin (GRBL sometimes starts with z not 0)
-		self.setOrigin()	
+class Cnc:
+    """CNC class
+    """
+    # Regular expression for parsing GRBL status msgs
+    __pos_pattern__ = re.compile(r".Pos:(\-?\d+\.\d+),(\-?\d+\.\d+),(\-?\d+\.\d+)")
 
-	def shutdown(self):
-		# close the serial connection
-		self.s.close()
-		
-	def getPos(self):
-		""" return a list [x,y,z] of the position of the gantry head """
-		return list(self.pos)	# copy the list so caller can't modify our internal state
-	
-	def getTwist(self):
+    def __init__(self):
+        self.serial = None  # serial port object
+        self.abs_move_mode = None  # GRBL has 2 movement modes, relative and absolute
+        self.baudrate = 0
+        self.port = ""
+        self.accel = 0
+        self.x_max = 0
+        self.y_max = 0
+        self.z_max = 0
+        self.default_speed = 0
+        self.x_max_speed = 0
+        self.y_max_speed = 0
+        self.z_max_speed = 0
+        # number of steps per centimeter in each dimension
+        self.x_steps_mm = 0
+        self.y_steps_mm = 0
+        self.z_steps_mm = 0
+        # machine idle
+        self.idle = True
+        # vectors follow the format [X, Y, Z] where Z is assumed to be vertical
+        self.pos = [0.0, 0.0, 0.0]  # current position
+        self.angular = [0.0, 0.0, 0.0]  # angular coordinates
+        self.origin = [0.0, 0.0, 0.0]  # minimum coordinates
+        self.limits = [0.0, 0.0, 0.0]  # maximum coordinates
 
-		#convert coordinates to ROS Twist format to be able to publish it later
-		cnc_pose = Twist()
-		cnc_pose.linear.x  = float(self.pos[0])
-		cnc_pose.linear.y  = float(self.pos[1])
-		cnc_pose.linear.z  = float(self.pos[2])
-		# this parameters are set to 0 as the cnc its a XYZ 3 DOF mechanism and doesnt need it
-		cnc_pose.angular.x = float(self.angular[0])
-		cnc_pose.angular.y = float(self.angular[1])
-		cnc_pose.angular.z = float(self.angular[2])
+    def startup(self, port, baudrate, accel, x_max, y_max, z_max, default_speed, x_max_speed,
+                y_max_speed, z_max_speed, x_steps_mm, y_steps_mm, z_steps_mm):
+        """Initiate CNC parameters read from .launch file
+        """
+        self.baudrate = baudrate
+        self.port = port
+        self.accel = accel
+        self.x_max = x_max
+        self.y_max = y_max
+        self.z_max = z_max
+        self.default_speed = default_speed
+        self.x_max_speed = x_max_speed
+        self.y_max_speed = y_max_speed
+        self.z_max_speed = z_max_speed
+        self.x_steps_mm = x_steps_mm
+        self.y_steps_mm = y_steps_mm
+        self.z_steps_mm = z_steps_mm
 
-		return cnc_pose
+        self.limits = [self.x_max, self.y_max, self.z_max]
+        self.serial = serial.Serial(self.port, self.baudrate)
+        self.ensure_move_mode(True)
+        self.home()
+        self.set_origin()
 
-	def setSpeed(self, speed):
+    def shutdown(self):
+        self.serial.close()
 
-		self.defaultSpeed = speed
-		
-	def home(self):
-		# initaites the home procedure
-		self.s.write("$H\n")
-		self.s.readline()
-		self.pos = list(self.origin)
+    def get_pos(self):
+        """Return a list [x,y,z] of the position of the gantry head
+        """
+        return list(self.pos)	# copy the list so caller can't modify our internal state
 
-	def enableSteppers(self):
-		# enable the stepper motors
-		try:
-			self.s.write("M17\n")
-			self.s.readline()
-		except:
-			print("Serial port unavailable")	
+    def get_twist(self):
+        # Convert coordinates to ROS Twist format to be able to publish it later
+        cnc_pos = Twist()
+        cnc_pos.linear.x = float(self.pos[0])
+        cnc_pos.linear.y = float(self.pos[1])
+        cnc_pos.linear.z = float(self.pos[2])
+        # These parameters are set to 0 as the CNC is a XYZ 3 DOF mechanism and doesnt need them
+        # ALO: ?
+        cnc_pos.angular.x = float(self.angular[0])
+        cnc_pos.angular.y = float(self.angular[1])
+        cnc_pos.angular.z = float(self.angular[2])
 
-	def disableSteppers(self):
-		# Disable the stepper motors
-		try:
-			self.s.write("M18\n")
-			self.s.readline()
-		except:
-			print("Serial port unavailable")
-	
-	def moveTo(self, x=None, y=None, z=None, speed=None, blockUntilComplete=True):
-		""" move to an absolute position, and return when movement completes """
-		if not self.idle: return
-		if x is None and y is None and z is None: return
-		if speed is None: speed = self.defaultSpeed
-		
-		self.ensureMovementMode(absoluteMode = True)
-		
-		gcode = 'G0'
-		letters = 'XYZ'
-		pos = (x, y, z)
-		newpos = list(self.pos)
-		
-		#create gcode string and update position list for each argument that isn't None
-		for i in range(3):
-			if pos[i] is not None:
-				#check against self.limits
-				if pos[i] < 0 or pos[i] >= self.limits[i]:
-					# if position is outside the movement range, ignore
-					return
-				gcode += ' ' + letters[i] + str(pos[i])
-				newpos[i] = pos[i]
+        return cnc_pos
 
-		gcode += ' F' + str(speed)
-		gcode += '\n'
-		try:
-			self.s.write(gcode)
-			self.s.readline()
-		except:
-			print("Serial port unavailable")
+    def set_speed(self, speed):
+        self.default_speed = speed
 
-	def moveRel(self, dx=None, dy=None, dz=None, speed=None, blockUntilComplete=True):
-		""" move a given distance, and return when movement completes
-		:param dx, dy, dz: distance to move
-		:param speed: units uncertain
-		:param blockUntilComplete: whether to return immediately, or wait for the movement to complete
-		"""
+    def home(self):
+        """Initiates the home procedure
+        """
+        self.serial.write("$H\n")
+        self.serial.readline()
+        self.pos = list(self.origin)
 
-		self.ensureMovementMode(absoluteMode = False)
-		if speed is None: speed = self.defaultSpeed
-		gcode = 'G0'
-		letters = 'xyz'
-		d = (dx, dy, dz)
-		newpos = list(self.pos)
-		
-		#create gcode string and update position list for each argument that isn't None (TODO: if successful?)
-		for i in range(3):
-			if d[i] is not None:
-				gcode += ' ' + letters[i] + str(d[i])
-				newpos[i] += d[i]
-		
-		gcode += ' f' + str(speed)
-		gcode += '\n'
-		
-		self.s.write(gcode)
-		self.s.readline()		
+    def enable_stepper_motors(self):
+        try:
+            self.serial.write("M17\n")
+            self.serial.readline()
+        except:
+            print("Serial port unavailable")
 
-		# the position update should be done after reading state
-		#update position if success
-		# TODO check to make sure it's actually a success
-		#self.pos = newpos
-		
-		if blockUntilComplete:
-			self.blockUntilIdle()
+    def disable_stepper_motors(self):
+        try:
+            self.serial.write("M18\n")
+            self.serial.readline()
+        except:
+            print("Serial port unavailable")
 
-	def moveToOrigin(self, speed = None):
-		""" move to starting position, and return when movement completes """
-		if speed is None: speed = self.defaultSpeed
-		self.moveTo(*self.origin, speed=speed)
-		self.pos = list(self.origin)
-			
-	def setOrigin(self, x=0, y=0, z=0):
-		"""set current position to be (0,0,0), or a custom (x,y,z)"""
-		gcode = "G92 x{} y{} z{}\n".format(x, y, z)
-		self.s.write(gcode)
-		self.s.readline()
-		
-		# update our internal location
-		self.pos = [x, y, z]
+    def move_to(self, x=None, y=None, z=None, speed=None):
+        """Move to an absolute position and return when movement completes
+        """
+        if not self.idle:
+            return
+        if x is None and y is None and z is None:
+            return
 
-	def ensureMovementMode(self, absoluteMode = True):
-		""" GRBL has two movement modes; if necessary this function tells GRBL to switch modes """
-		if self.abs_move == absoluteMode: return
-		
-		self.abs_move = absoluteMode
-		if absoluteMode:
-			self.s.write("G90\n")		# absolute movement mode
-		else:
-			self.s.write("G91\n")		# relative movement mode
-		self.s.readline()
-	
+        if speed is None:
+            speed = self.default_speed
 
-	def blockUntilIdle(self):
-		""" polls until GRBL indicates it is done with the last command """
-		pollcount = 0
-		while True:
-			self.s.write("?")
-			status = self.s.readline()
-			if status.startswith('<Idle'): break
-			# not used
-			pollcount += 1
-			# poll every 10 ms
-			time.sleep(.01)		
+        self.ensure_move_mode(abs_move_mode=True)
 
-		
-	def getStatus(self):
+        gcode = 'G0'
+        letters = 'XYZ'
+        pos = (x, y, z)
+        newpos = list(self.pos)
 
-		self.s.write("?")
-		
-		while True:
-			try: 
-				status = self.s.readline()
-				if status is not None:
-					try:
-						matches = self.__pos_pattern__.findall(status)
-						if len(matches[1]) == 3:
-							self.pos = list(matches[1])				
-						return status
-					except IndexError:
-						print("No matches found in serial")
-				else: break
-			except:
-				print("Report readiness but empty")
+        #create gcode string and update position list for each argument that isn't None
+        for i in range(3):
+            if pos[i] is not None:
+                #check against self.limits
+                if pos[i] < 0 or pos[i] >= self.limits[i]:
+                    # if position is outside the movement range, ignore
+                    return
+                gcode += ' ' + letters[i] + str(pos[i])
+                newpos[i] = pos[i]
+
+        gcode += ' F' + str(speed)
+        gcode += '\n'
+        try:
+            self.serial.write(gcode)
+            self.serial.readline()
+        except:
+            print("Serial port unavailable")
+
+    def move_relative(self, dx=None, dy=None, dz=None, speed=None, block=True):
+        """ move a given distance, and return when movement completes
+        :param dx, dy, dz: distance to move
+        :param speed: units uncertain
+        :param block: whether to return immediately, or wait for the movement to complete
+        """
+
+        self.ensure_move_mode(abs_move_mode=False)
+        if speed is None:
+            speed = self.default_speed
+
+        gcode = 'G0'
+        letters = 'xyz'
+        d = (dx, dy, dz)
+        newpos = list(self.pos)
+
+        # create gcode string and update position list for each argument that isn't None
+        # TODO (pablo): if successful?
+        for i in range(3):
+            if d[i] is not None:
+                gcode += ' ' + letters[i] + str(d[i])
+                newpos[i] += d[i]
+
+            gcode += ' f' + str(speed)
+            gcode += '\n'
+
+        self.serial.write(gcode)
+        self.serial.readline()
+
+        # the position update should be done after reading state
+        #update position if success
+        # TODO (pablo): check to make sure it's actually a success
+        #self.pos = newpos
+
+        if block:
+            self.block_until_idle()
+
+    def move_to_origin(self, speed=None):
+        """ Move to starting position, and return when movement completes
+        """
+        if speed is None:
+            speed = self.default_speed
+        self.move_to(*self.origin, speed=speed)
+        self.pos = list(self.origin)
+
+    def set_origin(self, x=0, y=0, z=0):
+        """ Set current position to be (0,0,0), or a custom (x,y,z)
+        """
+        gcode = "G92 x{} y{} z{}\n".format(x, y, z)
+        self.serial.write(gcode)
+        self.serial.readline()
+
+        # update our internal location
+        self.pos = [x, y, z]
+
+    def ensure_move_mode(self, abs_move_mode=True):
+        """ GRBL has two movement modes; if necessary this function tells GRBL to switch modes
+        """
+        if self.abs_move_mode == abs_move_mode: return
+
+        self.abs_move_mode = abs_move_mode
+        if abs_move_mode:
+            self.serial.write("G90\n")  # absolute movement mode
+        else:
+            self.serial.write("G91\n")  # relative movement mode
+            self.serial.readline()
+
+    def block_until_idle(self):
+        """ Polls until GRBL indicates it is done with the last command
+        """
+        pollcount = 0
+        while True:
+            self.serial.write("?")
+            status = self.serial.readline()
+            if status.startswith('<Idle'): break
+            # not used
+            pollcount += 1
+            # poll every 10 ms
+            time.sleep(.01)
+
+    def get_status(self):
+        self.serial.write("?")
+
+        while True:
+            try:
+                status = self.serial.readline()
+                if status is not None:
+                    try:
+                        matches = self.__pos_pattern__.findall(status)
+                        if len(matches[1]) == 3:
+                            self.pos = list(matches[1])
+                            return status
+                    except IndexError:
+                        print("No matches found in serial")
+                else: break
+            except:
+                print("Report readiness but empty")
--- src/cnc_interface/scripts/cnc_interface.py
+++ src/cnc_interface/scripts/cnc_interface.py
@@ -1,64 +1,63 @@
-#!/usr/bin/env python
+# cnc_interface.py
 
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-from cnc_class import cnc
+from cnc_class import Cnc
 
-cnc_obj = cnc()
+CNC_OBJ = Cnc()
 
-def cmdCallback(msg):
 
-	rospy.loginfo(rospy.get_name() + ": " + str(msg))
-	print msg.linear.x, msg.linear.y, msg.linear.z
-	cnc_obj.moveTo(msg.linear.x, msg.linear.y, msg.linear.z, blockUntilComplete=True)
+def cmd_callback(msg):
+    rospy.loginfo(rospy.get_name() + ": " + str(msg))
+    print(msg.linear.x, msg.linear.y, msg.linear.z)
+    CNC_OBJ.move_to(msg.linear.x, msg.linear.y, msg.linear.z)
 
-def stopCallback(msg):
-		#stop steppers
-	if   msg.data == 's':
-		cnc_obj.disableSteppers()
-		# fire steppers
-	elif msg.data == 'f':
-		cnc_obj.enableSteppers()
+def stop_callback(msg):
+    if msg.data == 's':
+        CNC_OBJ.disable_stepper_motors()
+    elif msg.data == 'f':
+        CNC_OBJ.enable_stepper_motors()
 
 def main():
+    """Create ROS topics
+    """
+    pos_pub = rospy.Publisher('/cnc_interface/position', Twist, queue_size=10)
+    status_pub = rospy.Publisher('/cnc_interface/status', String, queue_size=10)
+    rospy.Subscriber('cnc_interface/cmd', Twist, cmd_callback)
+    rospy.Subscriber('cnc_interface/stop', String, stop_callback)
 
-	''' create ROS topics '''
-	pos_pub     = rospy.Publisher('/cnc_interface/position',  Twist, queue_size = 10)
-	status_pub  = rospy.Publisher('/cnc_interface/status'  , String, queue_size = 10)
-	rospy.Subscriber('cnc_interface/cmd' ,  Twist,  cmdCallback)
-	rospy.Subscriber('cnc_interface/stop', String, stopCallback)
+    rospy.init_node('cnc_interface', anonymous=True)
 
-	rospy.init_node('cnc_interface', anonymous=True)
+    port = rospy.get_param('cnc_interface/port')
+    baudrate = rospy.get_param('cnc_interface/baudrate')
+    acc = rospy.get_param('cnc_interface/acceleration')
+    max_x = rospy.get_param('cnc_interface/x_max')
+    max_y = rospy.get_param('cnc_interface/y_max')
+    max_z = rospy.get_param('cnc_interface/x_max')
+    default_speed = rospy.get_param('cnc_interface/default_speed')
+    speed_x = rospy.get_param('cnc_interface/x_max_speed')
+    speed_y = rospy.get_param('cnc_interface/y_max_speed')
+    speed_z = rospy.get_param('cnc_interface/z_max_speed')
+    steps_x = rospy.get_param('cnc_interface/x_steps_mm')
+    steps_y = rospy.get_param('cnc_interface/y_steps_mm')
+    steps_z = rospy.get_param('cnc_interface/z_steps_mm')
 
-	port          = rospy.get_param('cnc_interface/port')
-	baud          = rospy.get_param('cnc_interface/baudrate')
-	acc           = rospy.get_param('cnc_interface/acceleration')  
-	max_x 		  = rospy.get_param('cnc_interface/x_max')   
-	max_y 		  = rospy.get_param('cnc_interface/y_max')   
-	max_z 		  = rospy.get_param('cnc_interface/x_max')
-	default_speed = rospy.get_param('cnc_interface/default_speed')   
-	speed_x  	  = rospy.get_param('cnc_interface/x_max_speed')
-	speed_y  	  = rospy.get_param('cnc_interface/y_max_speed')
-	speed_z  	  = rospy.get_param('cnc_interface/z_max_speed')
-	steps_x 	  = rospy.get_param('cnc_interface/x_steps_mm')
-	steps_y 	  = rospy.get_param('cnc_interface/y_steps_mm')
-	steps_z 	  = rospy.get_param('cnc_interface/z_steps_mm')
+    CNC_OBJ.startup(port, baudrate, acc, max_x, max_y, max_z, default_speed, speed_x, speed_y,
+                    speed_z, steps_x, steps_y, steps_z)
+    rate = rospy.Rate(10)
 
-	cnc_obj.startup(port,baud,acc,max_x,max_y,max_z,default_speed,speed_x,speed_y,
-					speed_z,steps_x,steps_y,steps_z)
-	rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        status = CNC_OBJ.get_status()
+        cnc_pose = CNC_OBJ.get_twist()
+        ros_status = String(status)
+        pos_pub.publish(cnc_pose)
+        status_pub.publish(ros_status)
+        rate.sleep()
 
-	while not rospy.is_shutdown():
+    rospy.spin()
 
-		status     = cnc_obj.getStatus()
-		cnc_pose   = cnc_obj.getTwist()
-		ros_status = String(status)
-		pos_pub.publish(cnc_pose)
-		status_pub.publish(ros_status)
-		rate.sleep()
 
-	rospy.spin()
-
-main()
+if __name__ == "__main__":
+    main()
