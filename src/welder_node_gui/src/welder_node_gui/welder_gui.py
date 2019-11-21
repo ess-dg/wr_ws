608c96a6f891660f59f9672af1b7dbf4c99b88db
anderslindho <anders.lindholsson@esss.se>
2019-11-21 17:03:14 +0100

    linting welder_node_gui pt3

--- src/welder_node_gui/src/welder_node_gui/welder_gui.py
+++ src/welder_node_gui/src/welder_node_gui/welder_gui.py
@@ -7,25 +7,31 @@ import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget, QLabel, QPushButton
+from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtGui import QImage, QPixmap
-from python_qt_binding.QtCore import Qt,Signal,Slot, QObject
+from python_qt_binding.QtCore import Qt, Signal
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import  String
 
-welder_states = ["Idle","Moving CNC to Next CV Point", "Awaiting CV result",
-                 "Awating Start Mark","Move to Point","Awating CNC to place",
+WELDER_STATES = ["Idle", "Moving CNC to Next CV Point", "Awaiting CV result",
+                 "Awating Start Mark", "Move to Point", "Awaiting CNC to place",
                  "Awating Laser Sync"]
+
 
 class MyPlugin(Plugin):
 
     def __init__(self, context):
         self.cmd_pub = rospy.Publisher("/welder/cmd", String, queue_size=1)
-        self.welder_status_sub = rospy.Subscriber('/welder/status', String, self.status_callback, queue_size=10)
+        self.welder_status_sub = rospy.Subscriber('/welder/status', String, self.status_callback,
+                queue_size=10)
         self.welder_time_sub = rospy.Subscriber('/welder/time', String, self.time_callback, queue_size=10)
-        self.welder_progress_sub = rospy.Subscriber('/welder/progress', String, self.progress_callback, queue_size=10)
-        self.image_sub = rospy.Subscriber("/laser_ctrl/img_feed/compressed", CompressedImage, self.image_callback, queue_size=10)
-        self.laser_state_sub = rospy.Subscriber('/laser_ctrl/cmd', String, self.laser_state_clbk, queue_size=10)
+        self.welder_progress_sub = rospy.Subscriber('/welder/progress', String, self.progress_callback,
+                queue_size=10)
+        self.image_sub = rospy.Subscriber("/laser_ctrl/img_feed/compressed", CompressedImage,
+                self.image_callback, queue_size=10)
+        self.laser_state_sub = rospy.Subscriber('/laser_ctrl/cmd', String, self.laser_state_clbk,
+                queue_size=10)
+
         super(MyPlugin, self).__init__(context)
 
         # Give QObjects reasonable names
@@ -51,10 +57,10 @@ class MyPlugin(Plugin):
         loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
         self._widget.setObjectName('Welder Robot GUI')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
+        # Show _widget.windowTitle on left-top of each plugin (when
+        # it's set in _widget). This is useful when you open multiple
+        # plugins at once. Also if you open multiple instances of your
+        # plugin at once, these lines add number to make it easy to
         # tell from pane to pane.
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
@@ -65,24 +71,24 @@ class MyPlugin(Plugin):
         self._widget.timeLabel.setText('None')
         #self._widget.progressBar.setValue(0)
         #self.progressSignal = Signal(str)
-        #self._widget.connect(self._widget,self.progressSignal,self.progressSlot)
+        #self._widget.connect(self._widget,self.progressSignal,self.progress_slot)
 
     def start_cnc(self):
         self.cmd_pub.publish('1')
 
-    def progressSlot(self,data):
-        blade_count = int(ros_data.data)
+    def progress_slot(self, data):
+        blade_count = int(ros_data.data) # this will not work, ros_data not defined
         progress = int(float(blade_count)/256*100)
         grids = int((blade_count/16))
         self._widget.progressBar.setValue(progress)
-        self._widget.bladeNumber.display(blade_count)       
-        self._widget.gridNumber.display(grids)   
+        self._widget.bladeNumber.display(blade_count)
+        self._widget.gridNumber.display(grids)
 
     def stop_cnc(self):
         self.cmd_pub.publish('0')
         self._widget.timeLabel.setText('None')
 
-    def image_callback(self,ros_data):
+    def image_callback(self, ros_data):
         np_arr = np.fromstring(ros_data.data, np.uint8)
         image = cv.imdecode(np_arr, cv.IMREAD_COLOR)
         height, width, _ = image.shape
@@ -113,7 +119,7 @@ class MyPlugin(Plugin):
         else:
             self._widget.startButton.setEnabled(True)
             self._widget.stopButton.setEnabled(False)
-        self._widget.statusLabel.setText(welder_states[int(ros_data.data)])
+        self._widget.statusLabel.setText(WELDER_STATES[int(ros_data.data)])
 
     def shutdown_plugin(self):
         # TODO (Pablo): unregister all publishers here
